4ccb935766fab023506007b15f15ea293c8d7f64
Sherio99 <139646562+Sherio99@users.noreply.github.com>
2023-07-16 20:16:53 +0530

    first commit

---
+++ AddUserVM.cs
@@ -0,0 +1,142 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Desktop01_3893.Model;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Desktop01_3893
+{
+    public partial class AddUserVM : ObservableObject
+
+    {
+        
+   
+        [ObservableProperty]
+        public string firstname;
+
+
+        [ObservableProperty]
+        public string lastname;
+
+        [ObservableProperty]
+        public int age;
+
+        [ObservableProperty]
+        public string dateofbirth;
+
+        [ObservableProperty]
+        public string gpa01;
+
+     
+
+        //To change the tile
+
+
+
+        [ObservableProperty]
+        public string title;
+
+        [ObservableProperty]
+        public BitmapImage selectedImage;
+
+      
+
+        public AddUserVM(User u)
+        {
+            Student = u;
+          
+            firstname  = Student.FirstName;
+            lastname = Student.LastName;
+            age = Student.Age;
+            gpa01 = Student.GPA;
+            dateofbirth = Student.DateOfBirth;
+            selectedImage=Student.Image;
+            
+        }
+        public AddUserVM()
+        {
+            
+        }
+
+
+        //get image 
+        [RelayCommand]
+        public void UploadPhoto()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files | *.bmp; *.png; *.jpg";
+            dialog.FilterIndex = 1;
+            if (dialog.ShowDialog() == true)
+            {
+                selectedImage = new BitmapImage(new Uri(dialog.FileName));
+               
+                MessageBox.Show("Image successfuly uploded!", "successfull");
+            }
+        }
+
+
+
+        
+
+
+        public User Student { get; private set; }
+        public Action CloseAction { get; internal set; }
+        
+        [RelayCommand]
+        public void Save()
+        {
+
+
+            double gpaDoub;
+            if (!double.TryParse(gpa01,out gpaDoub) || gpaDoub<0 ||gpaDoub>4) {
+                MessageBox.Show("GPA value must be between 0 and 4.", "Error" );
+                return;
+            }
+            if(Student==null)
+            {
+
+                Student = new User()
+                {
+                    FirstName = firstname,
+                    LastName = lastname,
+                    Age = age,
+                    DateOfBirth = dateofbirth,
+                    Image = selectedImage,
+
+                    GPA = gpa01
+
+                };
+
+
+            }
+            else
+            {
+                
+                Student.FirstName = firstname;
+                Student.LastName = lastname;
+                Student.Age = age;
+                Student.GPA = gpa01;
+                Student.Image = selectedImage;
+                Student.DateOfBirth = dateofbirth;
+                
+                
+                
+            }
+           
+            if(Student.FirstName != null ) { 
+
+                CloseAction(); }
+            Application.Current.MainWindow.Show();
+
+
+        }
+    }
+}
---
+++ MainWindowVM.cs
@@ -0,0 +1,109 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Desktop01_3893.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Desktop01_3893
+{
+    public  partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public  ObservableCollection<User> users;
+        [ObservableProperty]
+        public User selectedUser=null;
+
+
+
+        public void CloseMainWindow()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+
+
+
+        [RelayCommand]
+        public void messeag()
+        {
+
+            MessageBox.Show($"{selectedUser.FirstName} GPA value must be between 0 and 4.", "Error");
+        }
+
+        [RelayCommand]
+        public void AddStudent()
+        {
+            var vm = new AddUserVM();
+            vm.title = "ADD USER";
+            AddUserWindow window = new AddUserWindow(vm);
+            window.ShowDialog();
+
+            if (vm.Student.FirstName != null)
+            {
+                users.Add(vm.Student);
+            }
+        }
+
+        [RelayCommand]
+        public void Delete()
+        {
+            if (selectedUser != null)
+            {
+                string name = selectedUser.FirstName;
+                users.Remove(selectedUser);
+                MessageBox.Show($"{name} is Deleted successfuly.", "DELETED \a ");
+                
+            }
+            else
+            {
+                MessageBox.Show("Plese Select Student before Delete.", "Error");
+
+
+            }
+        }
+        [RelayCommand]
+        public void ExecuteEditStudentCommand()
+        {
+            if (selectedUser != null)
+            {
+                var vm = new AddUserVM(selectedUser);
+                vm.title = "EDIT USER";
+                var window = new AddUserWindow(vm);
+
+                window.ShowDialog();
+
+
+                int index = users.IndexOf(selectedUser);    
+                users.RemoveAt(index);
+                users.Insert(index, vm.Student);
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select the student", "Warning!");
+            }
+        }
+
+        public  MainWindowVM()
+        {
+            users = new ObservableCollection<User>();
+            BitmapImage image1 = new BitmapImage(new Uri("/Model/Images/1.png", UriKind.Relative));
+            users.Add(new User(23, "Sheranga", "Dias", "28/11/1999",image1,"3.4"));
+            BitmapImage image2 = new BitmapImage(new Uri("/Model/Images/2.png", UriKind.Relative));
+            users.Add(new User(18, "Chathura", "Anthony", "05/10/2005",image2,"3.7"));
+            BitmapImage image3 = new BitmapImage(new Uri("/Model/Images/3.png", UriKind.Relative));
+            users.Add(new User(26, "Sangeeth", "bandara", "20/2/1997",image3,"2.5"));
+            BitmapImage image4= new BitmapImage(new Uri("/Model/Images/4.png", UriKind.Relative));
+            users.Add(new User(20, "Shenali", "De Silva", "25/11/1993", image4,"1.6"));
+
+        }
+    }
+}
